72c29d9e1af6250d70200e19843f785b7d8d4499
stephen911 <stephendappah1@gmail.com>
2022-07-24 22:43:17 +0000

    done

--- index.php
+++ index.php
@@ -249,10 +249,10 @@
         <div class="sorting-menu">
           <ul>
             <li class="filter active" data-filter="all">All</li>
-            <li class="filter" data-filter=".web">New</li>
+            <!-- <li class="filter" data-filter=".web">New</li>
             <li class="filter" data-filter=".ui">Featured</li>
-            <li class="filter" data-filter=".branding">Recent</li>
-            <li class="filter" data-filter=".ux">Full Time</li>
+            <li class="filter" data-filter=".branding">Recent</li> -->
+            <li class="filter" data-filter=".ux">Internship</li>
             <li class="filter" data-filter=".wp">Part Time</li>
           </ul>
         </div>
@@ -282,9 +282,7 @@
                   </div>
                   <div class="job-inner-right">
                     <ul>
-                      <li>
-                        <a href="create-account.php">Apply</a>
-                      </li>
+                      
                       <li>
                         <span>Full Time</span>
                       </li>
@@ -315,9 +313,7 @@
                   </div>
                   <div class="job-inner-right">
                     <ul>
-                      <li>
-                        <a href="create-account.php">Apply</a>
-                      </li>
+                      
                       <li>
                         <span>Part Time</span>
                       </li>
@@ -350,9 +346,7 @@
                   </div>
                   <div class="job-inner-right">
                     <ul>
-                      <li>
-                        <a href="create-account.php">Apply</a>
-                      </li>
+                      
                       <li>
                         <span>Intern</span>
                       </li>
@@ -385,9 +379,7 @@
                   </div>
                   <div class="job-inner-right">
                     <ul>
-                      <li>
-                        <a href="create-account.php">Apply</a>
-                      </li>
+                      
                       <li>
                         <span>Part Time</span>
                       </li>
@@ -420,9 +412,7 @@
                   </div>
                   <div class="job-inner-right">
                     <ul>
-                      <li>
-                        <a href="create-account.php">Apply</a>
-                      </li>
+                      
                       <li>
                         <span>Intern</span>
                       </li>
@@ -455,9 +445,7 @@
                   </div>
                   <div class="job-inner-right">
                     <ul>
-                      <li>
-                        <a href="create-account.php">Apply</a>
-                      </li>
+                      
                       <li>
                         <span>Part Time</span>
                       </li>
@@ -488,9 +476,7 @@
                   </div>
                   <div class="job-inner-right">
                     <ul>
-                      <li>
-                        <a href="create-account.php">Apply</a>
-                      </li>
+                      
                       <li>
                         <span>Full Time</span>
                       </li>
@@ -521,9 +507,7 @@
                   </div>
                   <div class="job-inner-right">
                     <ul>
-                      <li>
-                        <a href="create-account.php">Apply</a>
-                      </li>
+                      
                       <li>
                         <span>Part Time</span>
                       </li>
@@ -589,7 +573,7 @@
       </div>
     </div>
 
-    <div class="portal-area pt-100 pb-70">
+    <!-- <div class="portal-area pt-100 pb-70">
       <div class="container">
         <div class="row align-items-center">
           <div class="col-lg-6">
@@ -668,7 +652,7 @@
           </div>
         </div>
       </div>
-    </div>
+    </div> -->
 
     <section class="companies-area ptb-100">
       <div class="container">
@@ -812,7 +796,7 @@
       </div>
     </section>
 
-    <div class="popular-area pt-100 pb-70">
+    <!-- <div class="popular-area pt-100 pb-70">
       <div class="container">
         <div class="row align-items-center">
           <div class="col-lg-6">
@@ -870,7 +854,7 @@
           </div>
         </div>
       </div>
-    </div>
+    </div> -->
 
     <section class="profile-area pb-100">
       <div class="container">
@@ -1074,7 +1058,7 @@
       </div>
     </section>
 
-    <div class="app-area">
+    <!-- <div class="app-area">
       <div class="container">
         <div class="row">
           <div class="col-lg-6">
@@ -1109,7 +1093,7 @@
           </div>
         </div>
       </div>
-    </div>
+    </div> -->
 
     <section class="blog-area pt-100">
       <div class="container">
@@ -1208,222 +1192,7 @@
       </div>
     </section>
 
-    <footer>
-      <div class="container">
-        <div class="subscribe-area">
-          <div class="section-title">
-            <h2>Subscribe Newsletter</h2>
-          </div>
-          <div class="subscribe-shape">
-            <img src="assets/img/shape/1.png" alt="Shape" />
-            <img src="assets/img/shape/1.png" alt="Shape" />
-            <img src="assets/img/shape/1.png" alt="Shape" />
-            <img src="assets/img/shape/1.png" alt="Shape" />
-            <img src="assets/img/shape/2.png" alt="Shape" />
-          </div>
-          <div class="subscribe-item">
-            <form class="newsletter-form" data-toggle="validator">
-              <input
-                type="email"
-                class="form-control"
-                placeholder="Enter Your Email"
-                name="EMAIL"
-                required
-                autocomplete="off"
-              />
-              <button class="btn subscribe-btn" type="submit">Subscribe</button>
-              <div id="validator-newsletter" class="form-result"></div>
-            </form>
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col-sm-6 col-lg-3">
-            <div class="footer-item">
-              <div class="footer-logo">
-                <a href="#">
-                  <img src="assets/img/logo-two.png" alt="Logo" />
-                </a>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                </p>
-                <ul>
-                  <li>
-                    <a href="https://www.youtube.com" target="_blank">
-                      <i class="icofont-youtube-play"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="https://www.facebook.com" target="_blank">
-                      <i class="icofont-facebook"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="https://www.instagram.com" target="_blank">
-                      <i class="icofont-instagram"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="https://www.twitter.com" target="_blank">
-                      <i class="icofont-twitter"></i>
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div class="col-sm-6 col-lg-3">
-            <div class="footer-item">
-              <div class="footer-category">
-                <h3>Category</h3>
-                <ul>
-                  <li>
-                    <a href="#">
-                      <i class="icofont-simple-right"></i>
-                      Development
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <i class="icofont-simple-right"></i>
-                      Business
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <i class="icofont-simple-right"></i>
-                      Tech & IT
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <i class="icofont-simple-right"></i>
-                      Finance
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <i class="icofont-simple-right"></i>
-                      Networking
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div class="col-sm-6 col-lg-3">
-            <div class="footer-item">
-              <div class="footer-category">
-                <h3>Quick Links</h3>
-                <ul>
-                  <li>
-                    <a href="index.php">
-                      <i class="icofont-simple-right"></i>
-                      Home
-                    </a>
-                  </li>
-                  <li>
-                    <a href="about.php">
-                      <i class="icofont-simple-right"></i>
-                      About Us
-                    </a>
-                  </li>
-                  <li>
-                    <a href="blog.php">
-                      <i class="icofont-simple-right"></i>
-                      Blogs
-                    </a>
-                  </li>
-                  <li>
-                    <a href="company-list.php">
-                      <i class="icofont-simple-right"></i>
-                      Companies
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <i class="icofont-simple-right"></i>
-                      Testimonials
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div class="col-sm-6 col-lg-3">
-            <div class="footer-item">
-              <div class="footer-find">
-                <h3>Find Us</h3>
-                <ul>
-                  <li>
-                    <i class="icofont-location-pin"></i>
-                    28/A Street, New York City
-                  </li>
-                  <li>
-                    <i class="icofont-ui-call"></i>
-                    <a href="tel:+880123456789"> +88 0123 456 789 </a>
-                  </li>
-                  <li>
-                    <i class="icofont-ui-email"></i>
-                    <a
-                      href="../../cdn-cgi/l/email-protection.php#bed6dbd2d2d1fed9dfdcd2db90ddd1d3"
-                    >
-                      <span
-                        class="__cf_email__"
-                        data-cfemail="96fef3fafaf9d6f1f7f4faf3b8f5f9fb"
-                        >[email&#160;protected]</span
-                      >
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="copyright-area">
-          <div class="row">
-            <div class="col-lg-6">
-              <div class="copyright-item">
-                <p>
-                  Copyright ©
-                  <script
-                    data-cfasync="false"
-                    src="../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"
-                  ></script>
-                  <script>
-                    document.write(new Date().getFullYear());
-                  </script>
-                  Gable. Designed By
-                  <a href="https://hibootstrap.com/" target="_blank"
-                    >HiBootstrap</a
-                  >
-                </p>
-              </div>
-            </div>
-            <div class="col-lg-6">
-              <div class="copyright-item copyright-right">
-                <ul>
-                  <li>
-                    <a href="terms-and-conditions.php" target="_blank"
-                      >Terms & Conditions</a
-                    >
-                  </li>
-                  <li>
-                    <span>-</span>
-                  </li>
-                  <li>
-                    <a href="privacy-policy.php" target="_blank"
-                      >Privacy Policy</a
-                    >
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </footer>
+    <?php include "footer.php" ?>
 
     <script src="assets/js/jquery.min.js"></script>
     <script src="assets/js/bootstrap.bundle.min.js"></script>
--- sidebar.php
+++ sidebar.php
@@ -1,180 +1,134 @@
 <div class="navbar-area fixed-top">
-      <div class="mobile-nav">
-        <a href="index.php" class="logo">
+  <div class="mobile-nav">
+    <a href="index.php" class="logo">
+      <img src="assets/img/logo.png" alt="Logo" />
+    </a>
+  </div>
+
+  <div class="main-nav">
+    <div class="container">
+      <nav class="navbar navbar-expand-md navbar-light">
+        <a class="navbar-brand" href="index.php">
           <img src="assets/img/logo.png" alt="Logo" />
         </a>
-      </div>
+        <div class="collapse navbar-collapse mean-menu" id="navbarSupportedContent">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a href="#" class="nav-link">Home</a>
+            </li>
 
-      <div class="main-nav">
-        <div class="container">
-          <nav class="navbar navbar-expand-md navbar-light">
-            <a class="navbar-brand" href="index.php">
-              <img src="assets/img/logo.png" alt="Logo" />
-            </a>
-            <div
-              class="collapse navbar-collapse mean-menu"
-              id="navbarSupportedContent"
-            >
-              <ul class="navbar-nav">
+            <li class="nav-item">
+              <a href="about.php" class="nav-link">Employer</a>
+            </li>
+
+            <li class="nav-item">
+              <a href="about.php" class="nav-link">Candidate</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link dropdown-toggle">Resources <i class="icofont-simple-down"></i></a>
+              <ul class="dropdown-menu">
                 <li class="nav-item">
-                  <a href="#" class="nav-link dropdown-toggle active"
-                    >Home <i class="icofont-simple-down"></i
-                  ></a>
-                  <ul class="dropdown-menu">
-                    <li class="nav-item">
-                      <a href="index.php" class="nav-link">Home Page 1</a>
-                    </li>
-                    <li class="nav-item">
-                      <a href="index-2.php" class="nav-link active"
-                        >Home Page 2</a
-                      >
-                    </li>
-                    <li class="nav-item">
-                      <a href="index-3.php" class="nav-link">Home Page 3</a>
-                    </li>
-                  </ul>
+                  <a href="job-list.php" class="nav-link">Blog</a>
                 </li>
                 <li class="nav-item">
-                  <a href="about.php" class="nav-link">About</a>
+                  <a href="favourite-job.php" class="nav-link">Events</a>
                 </li>
                 <li class="nav-item">
-                  <a href="#" class="nav-link dropdown-toggle"
-                    >Jobs <i class="icofont-simple-down"></i
-                  ></a>
-                  <ul class="dropdown-menu">
-                    <li class="nav-item">
-                      <a href="job-list.php" class="nav-link">Job List</a>
-                    </li>
-                    <li class="nav-item">
-                      <a href="favourite-job.php" class="nav-link"
-                        >Favourite Jobs</a
-                      >
-                    </li>
-                    <li class="nav-item">
-                      <a href="job-details.php" class="nav-link"
-                        >Job Details</a
-                      >
-                    </li>
-                    <li class="nav-item">
-                      <a href="post-a-job.php" class="nav-link">Post A Job</a>
-                    </li>
-                  </ul>
+                  <a href="job-details.php" class="nav-link">Courses</a>
                 </li>
                 <li class="nav-item">
-                  <a href="#" class="nav-link dropdown-toggle"
-                    >Candidates <i class="icofont-simple-down"></i
-                  ></a>
-                  <ul class="dropdown-menu">
-                    <li class="nav-item">
-                      <a href="candidate-list.php" class="nav-link"
-                        >Candidate List</a
-                      >
-                    </li>
-                    <li class="nav-item">
-                      <a href="candidate-details.php" class="nav-link"
-                        >Candidate Details</a
-                      >
-                    </li>
-                    <li class="nav-item">
-                      <a href="single-resume.php" class="nav-link"
-                        >Single Resume</a
-                      >
-                    </li>
-                    <li class="nav-item">
-                      <a href="submit-resume.php" class="nav-link"
-                        >Submit Resume</a
-                      >
-                    </li>
-                    <li class="nav-item">
-                      <a href="pricing.php" class="nav-link">Pricing</a>
-                    </li>
-                    <li class="nav-item">
-                      <a href="dashboard.php" class="nav-link"
-                        >Candidate dashboard</a
-                      >
-                    </li>
-                  </ul>
+                  <a href="post-a-job.php" class="nav-link">Skills Assesment</a>
                 </li>
                 <li class="nav-item">
-                  <a href="#" class="nav-link dropdown-toggle"
-                    >Pages <i class="icofont-simple-down"></i
-                  ></a>
-                  <ul class="dropdown-menu">
-                    <li class="nav-item">
-                      <a href="company-list.php" class="nav-link"
-                        >Company List</a
-                      >
-                    </li>
-                    <li class="nav-item">
-                      <a href="company-details.php" class="nav-link"
-                        >Company Details</a
-                      >
-                    </li>
-                    <li class="nav-item">
-                      <a href="login.php" class="nav-link">Login Page</a>
-                    </li>
-                    <li class="nav-item">
-                      <a href="create-account.php" class="nav-link"
-                        >Create Account Page</a
-                      >
-                    </li>
-                    <li class="nav-item">
-                      <a href="profile.php" class="nav-link">Profile</a>
-                    </li>
-                    <li class="nav-item">
-                      <a href="single-profile.php" class="nav-link"
-                        >Single Profile</a
-                      >
-                    </li>
-                    <li class="nav-item">
-                      <a href="404.php" class="nav-link">404</a>
-                    </li>
-                    <li class="nav-item">
-                      <a href="faq.php" class="nav-link">FAQ</a>
-                    </li>
-                    <li class="nav-item">
-                      <a href="terms-and-conditions.php" class="nav-link"
-                        >Terms and Conditions</a
-                      >
-                    </li>
-                    <li class="nav-item">
-                      <a href="privacy-policy.php" class="nav-link"
-                        >Privacy Policy</a
-                      >
-                    </li>
-                  </ul>
-                </li>
-                <li class="nav-item">
-                  <a href="#" class="nav-link dropdown-toggle"
-                    >Blogs <i class="icofont-simple-down"></i
-                  ></a>
-                  <ul class="dropdown-menu">
-                    <li class="nav-item">
-                      <a href="blog.php" class="nav-link">Blog</a>
-                    </li>
-                    <li class="nav-item">
-                      <a href="blog-details.php" class="nav-link"
-                        >Blog Details</a
-                      >
-                    </li>
-                  </ul>
-                </li>
-                <li class="nav-item">
-                  <a href="contact.php" class="nav-link">Contact</a>
+                  <a href="post-a-job.php" class="nav-link">CV Review</a>
                 </li>
               </ul>
-              <div class="common-btn">
-                <a class="login-btn" href="login.php">
-                  <i class="icofont-plus-square"></i>
-                  Login
-                </a>
-                <a class="sign-up-btn" href="create-account.php">
-                  <i class="icofont-user-alt-4"></i>
-                  Sign Up
-                </a>
-              </div>
-            </div>
-          </nav>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link dropdown-toggle">About <i class="icofont-simple-down"></i></a>
+              <ul class="dropdown-menu">
+                <li class="nav-item">
+                  <a href="candidate-list.php" class="nav-link">About Us</a>
+                </li>
+                <li class="nav-item">
+                  <a href="candidate-details.php" class="nav-link">FAQs</a>
+                </li>
+                <li class="nav-item">
+                  <a href="single-resume.php" class="nav-link">Terms and Services</a>
+                </li>
+                <li class="nav-item">
+                  <a href="submit-resume.php" class="nav-link">Privacy Policy</a>
+                </li>
+                <li class="nav-item">
+                  <a href="pricing.php" class="nav-link">Pricing</a>
+                </li>
+                <li class="nav-item">
+                  <a href="dashboard.php" class="nav-link">Candidate dashboard</a>
+                </li>
+              </ul>
+            </li>
+            <!-- <li class="nav-item">
+              <a href="#" class="nav-link dropdown-toggle">Pages <i class="icofont-simple-down"></i></a>
+              <ul class="dropdown-menu">
+                <li class="nav-item">
+                  <a href="company-list.php" class="nav-link">Company List</a>
+                </li>
+                <li class="nav-item">
+                  <a href="company-details.php" class="nav-link">Company Details</a>
+                </li>
+                <li class="nav-item">
+                  <a href="login.php" class="nav-link">Login Page</a>
+                </li>
+                <li class="nav-item">
+                  <a href="create-account.php" class="nav-link">Create Account Page</a>
+                </li>
+                <li class="nav-item">
+                  <a href="profile.php" class="nav-link">Profile</a>
+                </li>
+                <li class="nav-item">
+                  <a href="single-profile.php" class="nav-link">Single Profile</a>
+                </li>
+                <li class="nav-item">
+                  <a href="404.php" class="nav-link">404</a>
+                </li>
+                <li class="nav-item">
+                  <a href="faq.php" class="nav-link">FAQ</a>
+                </li>
+                <li class="nav-item">
+                  <a href="terms-and-conditions.php" class="nav-link">Terms and Conditions</a>
+                </li>
+                <li class="nav-item">
+                  <a href="privacy-policy.php" class="nav-link">Privacy Policy</a>
+                </li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link dropdown-toggle">Blogs <i class="icofont-simple-down"></i></a>
+              <ul class="dropdown-menu">
+                <li class="nav-item">
+                  <a href="blog.php" class="nav-link">Blog</a>
+                </li>
+                <li class="nav-item">
+                  <a href="blog-details.php" class="nav-link">Blog Details</a>
+                </li>
+              </ul>
+            </li> -->
+            <li class="nav-item">
+              <a href="contact.php" class="nav-link">Contact</a>
+            </li>
+          </ul>
+          <div class="common-btn">
+            <a class="login-btn" href="login.php">
+              <i class="icofont-plus-square"></i>
+              Login
+            </a>
+            <a class="sign-up-btn" href="create-account.php">
+              <i class="icofont-user-alt-4"></i>
+              Sign Up
+            </a>
+          </div>
         </div>
-      </div>
+      </nav>
     </div>
+  </div>
+</div>
